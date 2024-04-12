e822fd8903088478d260ac0c27a876606019cb6c
helmesjo <helmesjo@gmail.com>
2024-04-12 11:54:18 +0200

    TMP: bdep new --package -l c++ -t lib,prefix-include=include,prefix-source=core,subdir=src,buildfile-in-prefix,third-party libopencv-core

---
+++ libopencv-core/tests/basics/driver.cxx
@@ -0,0 +1,34 @@
+#include <sstream>
+#include <stdexcept>
+
+#include <src/opencv-core.hxx>
+
+#undef NDEBUG
+#include <cassert>
+
+int main ()
+{
+  using namespace std;
+  using namespace opencv_core;
+
+  // Basics.
+  //
+  {
+    ostringstream o;
+    say_hello (o, "World");
+    assert (o.str () == "Hello, World!\n");
+  }
+
+  // Empty name.
+  //
+  try
+  {
+    ostringstream o;
+    say_hello (o, "");
+    assert (false);
+  }
+  catch (const invalid_argument& e)
+  {
+    assert (e.what () == string ("empty name"));
+  }
+}
