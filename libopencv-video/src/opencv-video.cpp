b73c371f5362985717e998a0884875bb103b6e1f
helmesjo <helmesjo@gmail.com>
2026-08-29 13:53:22 +0200

    bdep new --package -l c++,cpp -t lib,split,subdir=opencv2,no-subdir-source,buildfile-in-prefix,third-party libopencv-video

---
+++ libopencv-video/src/opencv-video.cpp
@@ -0,0 +1,17 @@
+#include <opencv2/opencv-video.hpp>
+
+#include <ostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace opencv_video
+{
+  void say_hello (ostream& o, const string& n)
+  {
+    if (n.empty ())
+      throw invalid_argument ("empty name");
+
+    o << "Hello, " << n << '!' << endl;
+  }
+}
